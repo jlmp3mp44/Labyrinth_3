f574ac793e1ba8bfe41bfa3dcde2ce2551d023aa
Julya <julya.meleshko@gmail.com>
2023-07-21 14:29:32 +0300

    Try to add CustomControl

--- Labyrinth_3/Level2.h
+++ Labyrinth_3/Level2.h
@@ -34,6 +34,30 @@ namespace Labyrinth3 {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::PictureBox^ pictureBox6;
+	protected:
+	private: System::Windows::Forms::PictureBox^ pictureBox5;
+	private: System::Windows::Forms::PictureBox^ pictureBox4;
+	private: System::Windows::Forms::PictureBox^ pictureBox3;
+	private: System::Windows::Forms::PictureBox^ pictureBox2;
+	private: System::Windows::Forms::PictureBox^ pictureBox1;
+	private: System::Windows::Forms::PictureBox^ cat;
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::FlowLayoutPanel^ Panel8;
+	private: System::Windows::Forms::FlowLayoutPanel^ flowLayoutPanel1;
+	private: System::Windows::Forms::FlowLayoutPanel^ flowLayoutPanel2;
+	private: System::Windows::Forms::FlowLayoutPanel^ flowLayoutPanel3;
+	private: System::Windows::Forms::FlowLayoutPanel^ flowLayoutPanel4;
+	private: System::Windows::Forms::FlowLayoutPanel^ flowLayoutPanel5;
+	private: System::Windows::Forms::FlowLayoutPanel^ flowLayoutPanel6;
+	private: System::Windows::Forms::FlowLayoutPanel^ flowLayoutPanel7;
+	private: System::Windows::Forms::FlowLayoutPanel^ flowLayoutPanel8;
+	private: System::Windows::Forms::FlowLayoutPanel^ flowLayoutPanel9;
+	private: System::Windows::Forms::FlowLayoutPanel^ flowLayoutPanel10;
+	private: System::Windows::Forms::FlowLayoutPanel^ flowLayoutPanel11;
+	private: System::Windows::Forms::FlowLayoutPanel^ flowLayoutPanel12;
+	private: System::Windows::Forms::FlowLayoutPanel^ flowLayoutPanel13;
+	private: System::Windows::Forms::FlowLayoutPanel^ flowLayoutPanel14;
 
 	private:
 		/// <summary>
@@ -48,12 +72,285 @@ namespace Labyrinth3 {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"Level2";
-			this->Padding = System::Windows::Forms::Padding(0);
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(Level2::typeid));
+			this->pictureBox6 = (gcnew System::Windows::Forms::PictureBox());
+			this->pictureBox5 = (gcnew System::Windows::Forms::PictureBox());
+			this->pictureBox4 = (gcnew System::Windows::Forms::PictureBox());
+			this->pictureBox3 = (gcnew System::Windows::Forms::PictureBox());
+			this->pictureBox2 = (gcnew System::Windows::Forms::PictureBox());
+			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			this->cat = (gcnew System::Windows::Forms::PictureBox());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->Panel8 = (gcnew System::Windows::Forms::FlowLayoutPanel());
+			this->flowLayoutPanel1 = (gcnew System::Windows::Forms::FlowLayoutPanel());
+			this->flowLayoutPanel2 = (gcnew System::Windows::Forms::FlowLayoutPanel());
+			this->flowLayoutPanel3 = (gcnew System::Windows::Forms::FlowLayoutPanel());
+			this->flowLayoutPanel4 = (gcnew System::Windows::Forms::FlowLayoutPanel());
+			this->flowLayoutPanel5 = (gcnew System::Windows::Forms::FlowLayoutPanel());
+			this->flowLayoutPanel6 = (gcnew System::Windows::Forms::FlowLayoutPanel());
+			this->flowLayoutPanel7 = (gcnew System::Windows::Forms::FlowLayoutPanel());
+			this->flowLayoutPanel8 = (gcnew System::Windows::Forms::FlowLayoutPanel());
+			this->flowLayoutPanel9 = (gcnew System::Windows::Forms::FlowLayoutPanel());
+			this->flowLayoutPanel10 = (gcnew System::Windows::Forms::FlowLayoutPanel());
+			this->flowLayoutPanel11 = (gcnew System::Windows::Forms::FlowLayoutPanel());
+			this->flowLayoutPanel12 = (gcnew System::Windows::Forms::FlowLayoutPanel());
+			this->flowLayoutPanel13 = (gcnew System::Windows::Forms::FlowLayoutPanel());
+			this->flowLayoutPanel14 = (gcnew System::Windows::Forms::FlowLayoutPanel());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox6))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox5))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox4))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox3))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox2))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->cat))->BeginInit();
+			this->SuspendLayout();
+			// 
+			// pictureBox6
+			// 
+			this->pictureBox6->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(230)), static_cast<System::Int32>(static_cast<System::Byte>(138)),
+				static_cast<System::Int32>(static_cast<System::Byte>(193)));
+			this->pictureBox6->Location = System::Drawing::Point(649, 206);
+			this->pictureBox6->Name = L"pictureBox6";
+			this->pictureBox6->Size = System::Drawing::Size(10, 352);
+			this->pictureBox6->TabIndex = 19;
+			this->pictureBox6->TabStop = false;
+			// 
+			// pictureBox5
+			// 
+			this->pictureBox5->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(230)), static_cast<System::Int32>(static_cast<System::Byte>(138)),
+				static_cast<System::Int32>(static_cast<System::Byte>(193)));
+			this->pictureBox5->Location = System::Drawing::Point(649, 38);
+			this->pictureBox5->Name = L"pictureBox5";
+			this->pictureBox5->Size = System::Drawing::Size(10, 82);
+			this->pictureBox5->TabIndex = 18;
+			this->pictureBox5->TabStop = false;
+			// 
+			// pictureBox4
+			// 
+			this->pictureBox4->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(230)), static_cast<System::Int32>(static_cast<System::Byte>(138)),
+				static_cast<System::Int32>(static_cast<System::Byte>(193)));
+			this->pictureBox4->Location = System::Drawing::Point(29, 38);
+			this->pictureBox4->Name = L"pictureBox4";
+			this->pictureBox4->Size = System::Drawing::Size(10, 521);
+			this->pictureBox4->TabIndex = 17;
+			this->pictureBox4->TabStop = false;
+			// 
+			// pictureBox3
+			// 
+			this->pictureBox3->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(230)), static_cast<System::Int32>(static_cast<System::Byte>(138)),
+				static_cast<System::Int32>(static_cast<System::Byte>(193)));
+			this->pictureBox3->Location = System::Drawing::Point(29, 549);
+			this->pictureBox3->Name = L"pictureBox3";
+			this->pictureBox3->Size = System::Drawing::Size(79, 10);
+			this->pictureBox3->TabIndex = 16;
+			this->pictureBox3->TabStop = false;
+			// 
+			// pictureBox2
+			// 
+			this->pictureBox2->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(230)), static_cast<System::Int32>(static_cast<System::Byte>(138)),
+				static_cast<System::Int32>(static_cast<System::Byte>(193)));
+			this->pictureBox2->Location = System::Drawing::Point(217, 549);
+			this->pictureBox2->Name = L"pictureBox2";
+			this->pictureBox2->Size = System::Drawing::Size(443, 10);
+			this->pictureBox2->TabIndex = 15;
+			this->pictureBox2->TabStop = false;
+			// 
+			// pictureBox1
+			// 
+			this->pictureBox1->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(230)), static_cast<System::Int32>(static_cast<System::Byte>(138)),
+				static_cast<System::Int32>(static_cast<System::Byte>(193)));
+			this->pictureBox1->Location = System::Drawing::Point(29, 38);
+			this->pictureBox1->Name = L"pictureBox1";
+			this->pictureBox1->Size = System::Drawing::Size(630, 10);
+			this->pictureBox1->TabIndex = 14;
+			this->pictureBox1->TabStop = false;
+			// 
+			// cat
+			// 
+			this->cat->BackColor = System::Drawing::Color::Transparent;
+			this->cat->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"cat.Image")));
+			this->cat->Location = System::Drawing::Point(110, 497);
+			this->cat->Name = L"cat";
+			this->cat->Size = System::Drawing::Size(101, 89);
+			this->cat->SizeMode = System::Windows::Forms::PictureBoxSizeMode::Zoom;
+			this->cat->TabIndex = 20;
+			this->cat->TabStop = false;
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->BackColor = System::Drawing::Color::Violet;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft YaHei", 22.2F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label1->Location = System::Drawing::Point(384, 584);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(164, 50);
+			this->label1->TabIndex = 21;
+			this->label1->Text = L"LEVEL 2";
+			// 
+			// Panel8
+			// 
+			this->Panel8->BackColor = System::Drawing::SystemColors::ControlDarkDark;
+			this->Panel8->Location = System::Drawing::Point(100, 401);
+			this->Panel8->Name = L"Panel8";
+			this->Panel8->Size = System::Drawing::Size(125, 41);
+			this->Panel8->TabIndex = 22;
+			// 
+			// flowLayoutPanel1
+			// 
+			this->flowLayoutPanel1->BackColor = System::Drawing::SystemColors::ControlDarkDark;
+			this->flowLayoutPanel1->Location = System::Drawing::Point(180, 298);
+			this->flowLayoutPanel1->Name = L"flowLayoutPanel1";
+			this->flowLayoutPanel1->Size = System::Drawing::Size(45, 131);
+			this->flowLayoutPanel1->TabIndex = 23;
+			// 
+			// flowLayoutPanel2
+			// 
+			this->flowLayoutPanel2->BackColor = System::Drawing::SystemColors::ControlDarkDark;
+			this->flowLayoutPanel2->Location = System::Drawing::Point(98, 281);
+			this->flowLayoutPanel2->Name = L"flowLayoutPanel2";
+			this->flowLayoutPanel2->Size = System::Drawing::Size(127, 42);
+			this->flowLayoutPanel2->TabIndex = 24;
+			// 
+			// flowLayoutPanel3
+			// 
+			this->flowLayoutPanel3->BackColor = System::Drawing::SystemColors::ControlDarkDark;
+			this->flowLayoutPanel3->Location = System::Drawing::Point(180, 120);
+			this->flowLayoutPanel3->Name = L"flowLayoutPanel3";
+			this->flowLayoutPanel3->Size = System::Drawing::Size(45, 172);
+			this->flowLayoutPanel3->TabIndex = 25;
+			// 
+			// flowLayoutPanel4
+			// 
+			this->flowLayoutPanel4->BackColor = System::Drawing::SystemColors::ControlDarkDark;
+			this->flowLayoutPanel4->Location = System::Drawing::Point(315, 412);
+			this->flowLayoutPanel4->Name = L"flowLayoutPanel4";
+			this->flowLayoutPanel4->Size = System::Drawing::Size(45, 131);
+			this->flowLayoutPanel4->TabIndex = 26;
+			// 
+			// flowLayoutPanel5
+			// 
+			this->flowLayoutPanel5->BackColor = System::Drawing::SystemColors::ControlDarkDark;
+			this->flowLayoutPanel5->Location = System::Drawing::Point(315, 281);
+			this->flowLayoutPanel5->Name = L"flowLayoutPanel5";
+			this->flowLayoutPanel5->Size = System::Drawing::Size(45, 131);
+			this->flowLayoutPanel5->TabIndex = 7;
+			// 
+			// flowLayoutPanel6
+			// 
+			this->flowLayoutPanel6->BackColor = System::Drawing::SystemColors::ControlDarkDark;
+			this->flowLayoutPanel6->Location = System::Drawing::Point(315, 161);
+			this->flowLayoutPanel6->Name = L"flowLayoutPanel6";
+			this->flowLayoutPanel6->Size = System::Drawing::Size(45, 131);
+			this->flowLayoutPanel6->TabIndex = 7;
+			// 
+			// flowLayoutPanel7
+			// 
+			this->flowLayoutPanel7->BackColor = System::Drawing::SystemColors::ControlDarkDark;
+			this->flowLayoutPanel7->Location = System::Drawing::Point(315, 370);
+			this->flowLayoutPanel7->Name = L"flowLayoutPanel7";
+			this->flowLayoutPanel7->Size = System::Drawing::Size(123, 42);
+			this->flowLayoutPanel7->TabIndex = 27;
+			// 
+			// flowLayoutPanel8
+			// 
+			this->flowLayoutPanel8->BackColor = System::Drawing::SystemColors::ControlDarkDark;
+			this->flowLayoutPanel8->Location = System::Drawing::Point(434, 54);
+			this->flowLayoutPanel8->Name = L"flowLayoutPanel8";
+			this->flowLayoutPanel8->Size = System::Drawing::Size(45, 131);
+			this->flowLayoutPanel8->TabIndex = 28;
+			// 
+			// flowLayoutPanel9
+			// 
+			this->flowLayoutPanel9->BackColor = System::Drawing::SystemColors::ControlDarkDark;
+			this->flowLayoutPanel9->Location = System::Drawing::Point(434, 180);
+			this->flowLayoutPanel9->Name = L"flowLayoutPanel9";
+			this->flowLayoutPanel9->Size = System::Drawing::Size(45, 131);
+			this->flowLayoutPanel9->TabIndex = 29;
+			// 
+			// flowLayoutPanel10
+			// 
+			this->flowLayoutPanel10->BackColor = System::Drawing::SystemColors::ControlDarkDark;
+			this->flowLayoutPanel10->Location = System::Drawing::Point(557, 264);
+			this->flowLayoutPanel10->Name = L"flowLayoutPanel10";
+			this->flowLayoutPanel10->Size = System::Drawing::Size(45, 131);
+			this->flowLayoutPanel10->TabIndex = 30;
+			// 
+			// flowLayoutPanel11
+			// 
+			this->flowLayoutPanel11->BackColor = System::Drawing::SystemColors::ControlDarkDark;
+			this->flowLayoutPanel11->Location = System::Drawing::Point(434, 370);
+			this->flowLayoutPanel11->Name = L"flowLayoutPanel11";
+			this->flowLayoutPanel11->Size = System::Drawing::Size(45, 102);
+			this->flowLayoutPanel11->TabIndex = 31;
+			// 
+			// flowLayoutPanel12
+			// 
+			this->flowLayoutPanel12->BackColor = System::Drawing::SystemColors::ControlDarkDark;
+			this->flowLayoutPanel12->Location = System::Drawing::Point(557, 264);
+			this->flowLayoutPanel12->Name = L"flowLayoutPanel12";
+			this->flowLayoutPanel12->Size = System::Drawing::Size(94, 47);
+			this->flowLayoutPanel12->TabIndex = 32;
+			// 
+			// flowLayoutPanel13
+			// 
+			this->flowLayoutPanel13->BackColor = System::Drawing::SystemColors::ControlDarkDark;
+			this->flowLayoutPanel13->Location = System::Drawing::Point(557, 385);
+			this->flowLayoutPanel13->Name = L"flowLayoutPanel13";
+			this->flowLayoutPanel13->Size = System::Drawing::Size(45, 131);
+			this->flowLayoutPanel13->TabIndex = 33;
+			// 
+			// flowLayoutPanel14
+			// 
+			this->flowLayoutPanel14->BackColor = System::Drawing::SystemColors::ControlDarkDark;
+			this->flowLayoutPanel14->Location = System::Drawing::Point(63, 401);
+			this->flowLayoutPanel14->Name = L"flowLayoutPanel14";
+			this->flowLayoutPanel14->Size = System::Drawing::Size(45, 131);
+			this->flowLayoutPanel14->TabIndex = 34;
+			// 
+			// Level2
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->BackColor = System::Drawing::Color::RoyalBlue;
+			this->ClientSize = System::Drawing::Size(748, 659);
+			this->Controls->Add(this->flowLayoutPanel11);
+			this->Controls->Add(this->flowLayoutPanel14);
+			this->Controls->Add(this->flowLayoutPanel13);
+			this->Controls->Add(this->flowLayoutPanel10);
+			this->Controls->Add(this->flowLayoutPanel12);
+			this->Controls->Add(this->flowLayoutPanel9);
+			this->Controls->Add(this->flowLayoutPanel8);
+			this->Controls->Add(this->flowLayoutPanel7);
+			this->Controls->Add(this->flowLayoutPanel6);
+			this->Controls->Add(this->flowLayoutPanel5);
+			this->Controls->Add(this->flowLayoutPanel4);
+			this->Controls->Add(this->flowLayoutPanel3);
+			this->Controls->Add(this->flowLayoutPanel2);
+			this->Controls->Add(this->flowLayoutPanel1);
+			this->Controls->Add(this->Panel8);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->cat);
+			this->Controls->Add(this->pictureBox6);
+			this->Controls->Add(this->pictureBox5);
+			this->Controls->Add(this->pictureBox4);
+			this->Controls->Add(this->pictureBox3);
+			this->Controls->Add(this->pictureBox2);
+			this->Controls->Add(this->pictureBox1);
+			this->Name = L"Level2";
+			this->Text = L"Level2";
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox6))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox5))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox4))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox3))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox2))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->cat))->EndInit();
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
 		}
 #pragma endregion
+		
 	};
 }
